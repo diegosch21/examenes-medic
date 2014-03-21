208bb3fede65074ba94015ece893edf38392c57f
Diego Schwindt <diego.sch21@gmail.com>
2014-03-21 14:20:06 -0300

    datos

--- assets/Database/datos.sql
+++ assets/Database/datos.sql
@@ -1,11 +1,12 @@
 USE dcs_examenes;
 
 #alumnos
-INSERT INTO alumnos(lu_alu,apellido_alu,nom_alu,dni_alu) VALUES(100233,'Gómez','Fulano',37896524);
-INSERT INTO alumnos(lu_alu,apellido_alu,nom_alu,dni_alu) VALUES(110568,'González','Natalia',39852147);
-INSERT INTO alumnos(lu_alu,apellido_alu,nom_alu,dni_alu) VALUES(102137,'Prince','Martin',36893521);
-INSERT INTO alumnos(lu_alu,apellido_alu,nom_alu,dni_alu) VALUES(93256,'Muntz','Nelson',35987654);
-INSERT INTO alumnos(lu_alu,apellido_alu,nom_alu,dni_alu) VALUES(87546,'Jones','Jimbo',33843567);
+INSERT INTO alumnos(lu_alu,apellido_alu,nom_alu,dni_alu) VALUES
+	(100233,'Gómez','Fulano',37896524),
+	(110568,'González','Natalia',39852147),
+	(102137,'Prince','Martin',36893521),
+	(87546,'Jones','Jimbo',33843567),
+	(93256,'Muntz','Nelson',35987654);
 
 #docentes
 INSERT INTO docentes(leg_doc,pass,apellido_doc,nom_doc,dni_doc,email_doc, admin) 
@@ -18,42 +19,40 @@ INSERT INTO docentes(leg_doc,pass,apellido_doc,nom_doc,dni_doc)
 		VALUES(7865,MD5('7865'),'Pérez','Marcela',20568987); 	
 
 #carreras
-INSERT INTO carreras(cod_carr,nom_carr) VALUES (166,'Licenciatura en Enfermería');
-INSERT INTO carreras(cod_carr,nom_carr) VALUES (155,'Medicina');
+INSERT INTO carreras(cod_carr,nom_carr) VALUES
+	(166,'Licenciatura en Enfermería'),
+	(155,'Medicina');
 
 #catedras
-INSERT INTO catedras(cod_cat,cod_carr,nom_cat)
-		VALUES(20018,166,'Enfermería, Fundamentos, Prácticas y Tendencias II');
-INSERT INTO catedras(cod_cat,cod_carr,nom_cat)
-		VALUES(20020,166,'Enfermería Familiar I');
-INSERT INTO catedras(cod_cat,cod_carr,nom_cat)
-		VALUES(20059,155,'Examen general final de carrera');
+INSERT INTO catedras(cod_cat,cod_carr,nom_cat) VALUES
+	(20018,166,'Enfermería, Fundamentos, Prácticas y Tendencias II'),
+	(20020,166,'Enfermería Familiar I'),
+	(20059,155,'Examen general final de carrera');
 
 #Alumnos asociados a las catedras
-INSERT INTO alumnos_catedras(lu_alu,cod_cat,estado_alu_cat) VALUES(100233,20018,1);
-INSERT INTO alumnos_catedras(lu_alu,cod_cat,estado_alu_cat) VALUES(110568,20018,2);
-INSERT INTO alumnos_catedras(lu_alu,cod_cat,estado_alu_cat) VALUES(110568,20020,1);
+INSERT INTO alumnos_catedras(lu_alu,cod_cat,estado_alu_cat) VALUES
+	(100233,20018,1),
+	(110568,20018,2),
+	(110568,20020,1),
+	(102137,20059,1);
 INSERT INTO alumnos_catedras(lu_alu,cod_cat) VALUES(93256,20020);
-INSERT INTO alumnos_catedras(lu_alu,cod_cat,estado_alu_cat) VALUES(102137,20059,1);
 
 #Docentes asociados a las catedras
-INSERT INTO docentes_catedras(leg_doc,cod_cat,permiso_doc) VALUES (5201,20018,1);
-INSERT INTO docentes_catedras(leg_doc,cod_cat,permiso_doc) VALUES (7865,20018,0);
-INSERT INTO docentes_catedras(leg_doc,cod_cat,permiso_doc) VALUES (7865,20020,1);
+INSERT INTO docentes_catedras(leg_doc,cod_cat,permiso_doc) VALUES
+	(5201,20018,1),
+	(7865,20018,0),
+	(7865,20020,1);
 INSERT INTO docentes_catedras(leg_doc,cod_cat) VALUES (10325,20059);
 
 #Guia
 INSERT INTO guias (cod_cat,nro_guia,tit_guia) 
 		VALUES(20018,8,'Control de constantes vitales');
 
-	INSERT INTO descripciones(id_guia,nom_desc,contenido_desc)
-			VALUES(1,'Objetivo del taller','El estudiante será capaz  de controlar  constantes vitales.');
-	INSERT INTO descripciones(id_guia,nom_desc,contenido_desc)
-			VALUES(1,'Caso clínico','La señorita Mayra, de 20 años, ingresa al consultorio para control de presión arterial, pulso, frecuencia  respiratoria,  temperatura y dolor.');
-	INSERT INTO descripciones(id_guia,nom_desc,contenido_desc)
-			VALUES(1,'Escenario','Laboratorio de competencias profesionales.');
-	INSERT INTO descripciones(id_guia,nom_desc,contenido_desc)
-			VALUES(1,'Requerimientos','Usuario simulado, estetoscopio, esfigmomanómetro, reloj, termómetro digital, torundas de algodón, alcohol en gel, alcohol al 70%, bandeja, hojas de registro, bolígrafo, bolsa  roja, toallas descartables.');		
+	INSERT INTO descripciones(id_guia,nom_desc,contenido_desc) VALUES
+		(1,'Objetivo del taller','El estudiante será capaz  de controlar  constantes vitales.'),
+		(1,'Caso clínico','La señorita Mayra, de 20 años, ingresa al consultorio para control de presión arterial, pulso, frecuencia  respiratoria,  temperatura y dolor.'),
+		(1,'Escenario','Laboratorio de competencias profesionales.'),
+		(1,'Requerimientos','Usuario simulado, estetoscopio, esfigmomanómetro, reloj, termómetro digital, torundas de algodón, alcohol en gel, alcohol al 70%, bandeja, hojas de registro, bolígrafo, bolsa  roja, toallas descartables.');		
 
 	INSERT INTO items(nom_item)	VALUES('Se lava las manos al inicio del procedimiento.');
 	INSERT INTO items_guias(id_item,id_guia,nro_item) VALUES (LAST_INSERT_ID(),1,1);
@@ -145,42 +144,44 @@ INSERT INTO examenes (id_guia,lu_alu,leg_doc,calificacion,obs_exam)
 		VALUES(1,100233,5201,1,'Esta es una observación general del examen');
 
 	INSERT INTO items_examenes(id_item,id_exam,estado_item,obs_item) VALUES (1,1,0,'Esto es una observación');
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (2,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (3,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (4,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (5,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (6,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (7,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (8,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (9,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (10,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (11,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (12,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (13,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (14,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (15,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (16,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (17,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (18,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (19,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (20,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (21,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (22,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (23,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (24,1,0);
+	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES 
+		(2,1,1),
+		(3,1,0),
+		(4,1,1),
+		(5,1,0),
+		(6,1,1),
+		(7,1,1),
+		(8,1,1),
+		(9,1,1),
+		(10,1,0),
+		(11,1,1),
+		(12,1,1),
+		(13,1,0),
+		(14,1,1),
+		(15,1,0),
+		(16,1,0),
+		(17,1,1),
+		(18,1,1),
+		(19,1,1),
+		(20,1,1),
+		(21,1,1),
+		(22,1,0),
+		(23,1,1),
+		(24,1,0);
 	INSERT INTO items_examenes(id_item,id_exam,estado_item,obs_item) VALUES (25,1,-1,'No pudo ser observado');
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (26,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (27,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (28,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (29,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (30,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (31,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (32,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (33,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (34,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (35,1,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (36,1,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (37,1,1);
+	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES
+		(26,1,0),
+		(27,1,1),
+		(28,1,0),
+		(29,1,1),
+		(30,1,1),
+		(31,1,1),
+		(32,1,1),
+		(33,1,0),
+		(34,1,0),
+		(35,1,1),
+		(36,1,0),
+		(37,1,1);
 
 #Guia medicina
 INSERT INTO guias (cod_cat,nro_guia,tit_guia,subtit_guia) 
@@ -251,23 +252,24 @@ INSERT INTO examenes (id_guia,lu_alu,leg_doc,calificacion,obs_exam)
 		VALUES(2,102137,10325,0,'Esta es una observación general del examen');
 
 	INSERT INTO items_examenes(id_item,id_exam,estado_item,obs_item) VALUES (38,2,0,'Esto es una observación');
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (39,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (40,2,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (41,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (42,2,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (43,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (44,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (45,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (46,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (47,2,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (48,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (49,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (50,2,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (51,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (52,2,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (53,2,0);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (54,2,1);
-	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (55,2,1);
+	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES
+		(39,2,1),
+		(40,2,0),
+		(41,2,1),
+		(42,2,0),
+		(43,2,1),
+		(44,2,1),
+		(45,2,1),
+		(46,2,1),
+		(47,2,0),
+		(48,2,1),
+		(49,2,1),
+		(50,2,0),
+		(51,2,1),
+		(52,2,0),
+		(53,2,0),
+		(54,2,1),
+		(55,2,1);
 	INSERT INTO items_examenes(id_item,id_exam,obs_item) VALUES (56,2,'Bla bla bla'); #solo texto
 	INSERT INTO items_examenes(id_item,id_exam,estado_item) VALUES (57,2,1);
 	INSERT INTO items_examenes(id_item,id_exam,obs_item) VALUES (58,2,'Bla bla bla'); #solo texto
